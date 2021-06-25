3563a018f9ee97268dc785902636fa61e2acf8b3
GitHub Action <action@github.com>
2021-06-25 02:27:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/xyaimm/testandroid/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/xyaimm/testandroid/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.xyaimm.testandroid.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xyaimm.testandroid.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/xyaimm/testandroid/services/MyProjectService.kt
+++ src/main/kotlin/com/github/xyaimm/testandroid/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.xyaimm.testandroid.services
 
+import com.github.xyaimm.testandroid.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
